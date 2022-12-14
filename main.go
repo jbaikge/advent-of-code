6403362d031b99c86e76a0528d1d1538afdb4468
jake <bigjake2s@gmail.com>
2022-12-14 13:09:17 -0500

    Adding timers to the part executions

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	hillclimb "github.com/jbaikge/advent-of-code/2022/12-hill-climb"
 	distress "github.com/jbaikge/advent-of-code/2022/13-distress"
@@ -55,11 +56,17 @@ func main() {
 		log.Fatalf("Unable to parse input: %v", err)
 	}
 
+	part1Start := time.Now()
 	if err = solution.Part1(os.Stdout); err != nil {
 		log.Fatalf("Unable to solve part 1: %v", err)
 	}
+	part1Took := time.Since(part1Start)
 
+	part2Start := time.Now()
 	if err = solution.Part2(os.Stdout); err != nil {
 		log.Fatalf("Unable to solve part 2: %v", err)
 	}
+	part2Took := time.Since(part2Start)
+
+	fmt.Printf("\nPart 1 took %s; Part 2 took %s\n", part1Took, part2Took)
 }
